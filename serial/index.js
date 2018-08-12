53b442283bc275144562f6a916c36ae47dfff2cf
Robert Spari <robert.spari@gmail.com>
2018-08-12 17:02:16 +0200

    fixes in event logic

--- serial/index.js
+++ serial/index.js
@@ -7,23 +7,18 @@ function Serial () {
 	this.port = new SerialPort('/dev/serial0', {baudRate: 38400})
 	this.combinedData = ''
 
-	this.handlers = {
-		data: []
-	}
+	this.handlers = []
 
-	this.subscribe = (fn) => this.handlers.data.push(fn)
+	this.subscribe = (fn) => this.handlers.push(fn)
 
 	this.unsubscribe = (fn) => this.handlers = this.handlers.filter(it => it !== fn)
 
 	this.port.on('readable', () => {
-		const theData = this.port.read()
-		const stringData = theData.toString('utf8')
+		this.combinedData += this.port.read().toString('utf8')
 
-		this.combinedData += stringData
-
-		if (stringData.indexOf(END_OF_LINE) > 0) {
+		if (this.combinedData.indexOf(END_OF_LINE) > 0) {
 			if (validate(this.combinedData))
-				this.handlers.forEach(handler => handler(stringData))
+				this.handlers.forEach(handler => handler(this.combinedData))
 			this.combinedData = ''
 		}
 
