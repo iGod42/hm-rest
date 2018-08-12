50d80baeeb33a6f3a584aae70f8f07d5c76cc928
Robert Spari <robert.spari@gmail.com>
2018-08-12 22:07:18 +0200

    bugfix for config request

--- hmApi/index.js
+++ hmApi/index.js
@@ -2,8 +2,10 @@ const Serial = require('./serial')
 
 const serial = Serial.getInstance()
 
+const requestSerialUrl = (url) => () => serail.write(`\n${url}\n`)
+
 module.exports = {
 	subscribe: serial.subscribe,
 	unsubscribe: serial.unsubscribe,
-	requestConfig: serial.write('\n/config\n')
+	requestConfig: () => requestSerialUrl(url)
 }
