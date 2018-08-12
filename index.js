c166dc0d9a5b55e136fd7f378e5f17952865da0f
Robert Spari <robert.spari@gmail.com>
2018-08-12 15:40:51 +0200

    added debug messages for all events

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const SerialPort = require('serialport')
 const port = new SerialPort('/dev/serial0')
 
+port.on('open', function() {
+	console.log('port opened')
+})
+
+port.on('close', function() {
+	console.log('port opened')
+})
+port.on('error', function() {
+	console.log('port opened')
+})
+
 
 port.on('readable', function () {
 	console.log('Data:', port.read());
