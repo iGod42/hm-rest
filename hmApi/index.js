d203f5b8417fcc2f1c9155f52a0e812e74b58b91
Robert Spari <robert.spari@gmail.com>
2018-08-14 10:23:19 +0200

    added possibility to store data

--- hmApi/index.js
+++ hmApi/index.js
@@ -1,18 +1,39 @@
+const EventEmitter = require('events')
+const Datastore = require('nedb')
+
 const llapi = require('./lowLevelApi')
 
-const handleUpdate = (record, config) => {
-	console.log(record)
+const db = new Datastore({filename: process.env.DB_PATH || './db', autoload: true})
+
+const saveRecord = (record) => {
+	const _record = {...record}
+	delete _record.code
+	delete _record.name
+	db.insert(_record, (err) => console.error)
+}
+
+const handleUpdate = (record, config, emit) => {
+	if (!record || !config)
+		return
+
+	switch (record.code) {
+		case 'HMSU':
+			saveRecord(record)
+			break;
+	}
 }
 
 const HmApi = (() => {
 	const llApiKey = Symbol()
 	return class HmApi extends EventEmitter {
 		constructor () {
+			super()
+
 			this.config = {}
 
 			const apiInstance = new llapi.LLapi()
 
-			apiInstance.on('updateReceived', (record) => handleUpdate(record, this.config))
+			apiInstance.on('updateReceived', (record) => handleUpdate(record, this.config, this.emit))
 
 			apiInstance.requestConfig()
 
